9908223e9cf43d510cbf02f764222e0c83c6d1e2
Nguyen Thanh Minh <thanhminhspk05@gmail.com>
2024-11-21 23:20:05 +0700

    Add assests folder and Implementation Authentication Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center bg-amber-100">
+    <div>
       <div className="text-3xl text-brand">
         StoreIt - The only storage solution you need
       </div>
