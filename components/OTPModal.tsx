5e0f6d1d3f573351e3a3747f279a30574f63e5f8
Nguyen Thanh Minh <thanhminhspk05@gmail.com>
2024-11-24 15:41:31 +0700

    Implement Search Function

--- components/OTPModal.tsx
+++ components/OTPModal.tsx
@@ -37,12 +37,8 @@ const OtpModal = ({
     e.preventDefault();
     setIsLoading(true);
 
-    console.log({ accountId, password });
-
     try {
       const sessionId = await verifySecret({ accountId, password });
-
-      console.log({ sessionId });
 
       if (sessionId) router.push("/");
     } catch (error) {
